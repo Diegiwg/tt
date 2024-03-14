b866ec8f101c7d267c2b32fbded650b76166c919
Diegiwg <diegiwg@gmail.com>
2024-03-14 20:12:04 -0300

    refactor: better help descriptions

--- tt.go
+++ tt.go
@@ -2,57 +2,48 @@ package main
 
 import (
 	cli "github.com/Diegiwg/cli"
-	"github.com/Diegiwg/tt/cmd"
+	cmd "github.com/Diegiwg/tt/cmd"
 )
-
-// tt start -> Cria um novo record na DB, salvando o tempo que o comando foi executado
-// tt pause -> Adiciona um tempo de pausa no record
-// tt resume -> Adiciona um marcador de tempo para voltar a contar
-// tt stop -> Finaliza o record
-// tt msg <msg> -> Adiciona uma mensagem ao record atual
-// tt status -> Mostra o status atual
-// tt list -> Mostra todos os records
-// tt show <id> -> Mostra um record
 
 func main() {
 	app := cli.NewApp()
 
 	app.AddCommand(&cli.Command{
 		Name:  "start",
-		Desc:  "Start a record of time",
-		Help:  "Start a record of time",
+		Desc:  "Starts a new time record",
+		Help:  "Clears the database and starts a new time record.",
 		Usage: "",
 		Exec:  cmd.Start,
 	})
 
 	app.AddCommand(&cli.Command{
 		Name:  "pause",
-		Desc:  "Pause a record of time",
-		Help:  "Pause a record of time",
+		Desc:  "Adds a pause to the time record",
+		Help:  "When you want to take a break, use this command.",
 		Usage: "",
 		Exec:  cmd.Pause,
 	})
 
 	app.AddCommand(&cli.Command{
 		Name:  "resume",
-		Desc:  "Resume a record of time",
-		Help:  "Resume a record of time",
+		Desc:  "Resumes counting time after a pause",
+		Help:  "When you want to resume after a pause, use this command.",
 		Usage: "",
 		Exec:  cmd.Resume,
 	})
 
 	app.AddCommand(&cli.Command{
 		Name:  "stop",
-		Desc:  "Stop a record of time",
-		Help:  "Stop a record of time",
+		Desc:  "Stops time counting, clearing the time record",
+		Help:  "When you want to finish the time record, use this command.",
 		Usage: "",
 		Exec:  cmd.Stop,
 	})
 
 	app.AddCommand(&cli.Command{
 		Name:  "show",
-		Desc:  "Show a record of time",
-		Help:  "Show a record of time",
+		Desc:  "Shows the time passed in the time record",
+		Help:  "When you want to see how much time has passed without finishing the record, use this command.",
 		Usage: "",
 		Exec:  cmd.Show,
 	})
