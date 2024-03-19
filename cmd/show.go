a808cd7c7d17a71a6938e36cf94213059b85d399
Diegiwg <diegiwg@gmail.com>
2024-03-19 20:46:34 -0300

    feat: better ux for show cmd

--- cmd/show.go
+++ cmd/show.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/Diegiwg/cli"
 	"github.com/Diegiwg/tt/data"
@@ -14,8 +16,28 @@ func Show(ctx *cli.Context) error {
 		return errors.New("no items found in record")
 	}
 
-	time := r.TotalTime()
-	println("Total time:", time)
+	var acc time.Duration
+
+	item_count := len(r.Items)
+	for index, item := range r.Items {
+		start := item.Start
+		end := item.End
+
+		if end == (time.Time{}) {
+
+			if index != item_count-1 {
+				continue
+			}
+
+			end = time.Now()
+		}
+
+		fmt.Printf("Item: %02d:%02d - %02d:%02d\n", start.Hour(), start.Minute(), end.Hour(), end.Minute())
+
+		acc += end.Sub(start)
+	}
+
+	println("Total time:", acc.String()+"\n")
 
 	return nil
 }
