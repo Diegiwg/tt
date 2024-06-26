fb52ecec9fd8106b917a90586d74669985d253c2
Diegiwg <diegiwg@gmail.com>
2024-06-26 09:49:45 -0300

    fix: add version check in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+# Get the last version in repository
+CURRENT_VERSION=$(git describe --abbrev=0 --tags)
+
 # Get the declared version in .version file
 VERSION=$(cat .version)
+
+# Check if versions are equal
+if [ "$CURRENT_VERSION" = "$VERSION" ]; then
+    echo "Versions are equal. Nothing to publish"
+    exit 0
+fi
 
 # Tidy the repository
 go mod tidy
@@ -19,7 +28,7 @@ GOPROXY=proxy.golang.org go list -m github.com/Diegiwg/tt@"$VERSION"
 
 # Test if the version was published
 echo "Getting the version from https://pkg.go.dev/github.com/Diegiwg/tt@$VERSION..."
-OK=$(curl -o /dev/null -s -w "%{http_code}\n" https://pkg.go.dev/github.com/Diegiwg/tt@"$VERSION")
+OK=$(curl -o /dev/null -X 'POST' -s -w "%{http_code}\n" https://pkg.go.dev/github.com/Diegiwg/tt@"$VERSION")
 
 if [ "$OK" = "200" ]; then
     echo "Version $VERSION was published"
