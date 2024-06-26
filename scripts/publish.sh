0befa7144fc74f81e277af44039c0b1274f79f89
Diegiwg <diegiwg@gmail.com>
2024-06-26 09:45:18 -0300

    fix: publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,17 +2,8 @@
 
 set -e
 
-# Get the last version in repository
-CURRENT_VERSION=$(git describe --abbrev=0 --tags)
-
 # Get the declared version in .version file
 VERSION=$(cat .version)
-
-# Check if versions are equal
-if [ "$CURRENT_VERSION" = "$VERSION" ]; then
-    echo "Versions are equal. Nothing to publish"
-    exit 0
-fi
 
 # Tidy the repository
 go mod tidy
@@ -27,6 +18,7 @@ git push origin "$VERSION"
 GOPROXY=proxy.golang.org go list -m github.com/Diegiwg/tt@"$VERSION"
 
 # Test if the version was published
+echo "Getting the version from https://pkg.go.dev/github.com/Diegiwg/tt@$VERSION..."
 OK=$(curl -o /dev/null -s -w "%{http_code}\n" https://pkg.go.dev/github.com/Diegiwg/tt@"$VERSION")
 
 if [ "$OK" = "200" ]; then
